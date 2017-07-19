7ba235ca1a985c08b527bf1b619a0c2961b689dc
Hyunjin Cho <hj.cho@regularbold.co.kr>
2017-07-19 15:14:09 +0900

    contents

---
+++ savethedeveloper/savethedeveloper_nb.js
@@ -0,0 +1,7 @@
+/* Save the developers Campaign */
+document.write('<!-- SAVE THE DEVELOPERS -->');
+document.write('<a href="http://resistan.com/savethedeveloper/" style="display:block\; display:inline-block\; position:relative\; white-space:nowrap\; width:auto\; margin:5px 0\; padding:7px\; border:1px solid #ccc\; line-height:normal\; text-decoration:none\; background:#FFF\;">');
+document.write('<span style="display:block\; font:18px/18px Arial\; color:#000\; margin:0\; padding:0\; letter-spacing:-1px">SAVE THE DEVELOPERS');
+document.write('<span style="position:absolute\; cursor:pointer\; display:block\; top:0\; right:5px\; font:40px/40px 'Arial Black'\; color:#000\; opacity:0.25\; filter:alpha(opacity=25);">&lt\;<span style="font:40px/40px 'Arial Black'\; color:#F00">!</span>&gt\;</span></span>');
+document.write('<span style="display:block\; font:10px/10px Verdana\; color:#F00\; margin:0\; padding:0\; letter-spacing:2px">Upgrade IE 6 Now!</span> </a>');
+document.write('<!-- /SAVE THE DEVELOPERS -->');
